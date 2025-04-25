57a8c010dbd8c6ac35820aa0509357e5bf7a0efa
mjgwon24 <alswlchlrh8@naver.com>
2025-04-25 09:36:48 +0900

    chore: CORS, Security, Swagger 공통 config 설정파일 추가

---
+++ src/main/java/com/flexrate/flexrate_back/common/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.flexrate.flexrate_back.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://front-domain.com")); // front-domain 생성 후 변경 필요
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
+
