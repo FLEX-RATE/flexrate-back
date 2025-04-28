cd12b4ba9001613707e20f470a2477120bb26df6
seocylucky <seocylucky@github.com>
2025-04-28 11:52:45 +0900

    fix: 대출 신청 요청 DTO의 필수 입력값 검증 수정

--- src/main/java/com/flexrate/flexrate_back/loan/dto/LoanPreApplicationRequest.java
+++ src/main/java/com/flexrate/flexrate_back/loan/dto/LoanPreApplicationRequest.java
@@ -1,10 +1,11 @@
 package com.flexrate.flexrate_back.loan.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
@@ -12,33 +13,33 @@ import lombok.NoArgsConstructor;
 @Builder
 public class LoanPreApplicationRequest {
 
-    @NotNull
+    @NotBlank(message = "업종(businessType)은 필수 입력 항목입니다.")
     private String businessType;
 
-    @NotNull
+    @NotBlank(message = "고용형태(employmentType)은 필수 입력 항목입니다.")
     private String employmentType;
 
-    @NotNull
+    @NotBlank(message = "입사일자(hireDate)는 필수 입력 항목입니다.")
     private String hireDate;
 
-    @NotNull
+    @NotBlank(message = "학교명(schoolName)은 필수 입력 항목입니다.")
     private String schoolName;
 
-    @NotNull
+    @NotBlank(message = "학적 상태(educationStatus)는 필수 입력 항목입니다.")
     private String educationStatus;
 
-    @NotNull
+    @NotNull(message = "연소득(annualIncome)은 필수 입력 항목입니다.")
     private Integer annualIncome;
 
-    @NotNull
+    @NotBlank(message = "신용등급(creditGrade)은 필수 입력 항목입니다.")
     private String creditGrade;
 
-    @NotNull
+    @NotBlank(message = "주거 형태(residenceType)는 필수 입력 항목입니다.")
     private String residenceType;
 
-    @NotNull
+    @NotNull(message = "개인회생 여부(isBankrupt)는 필수 입력 항목입니다.")
     private Boolean isBankrupt;
 
-    @NotNull
+    @NotBlank(message = "대출 목적(loanPurpose)은 필수 입력 항목입니다.")
     private String loanPurpose;
 }
