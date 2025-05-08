3c5b8547c97fe2d89ed6e681bdb2161b657b73ff
seocylucky <seocylucky@github.com>
2025-05-08 15:18:10 +0900

    feat: yearMonth 객체와 문자열 날짜 데이터와의 변환 코드 작업

---
+++ src/main/java/com/flexrate/flexrate_back/common/converter/YearMonthAttributeConverter.java
@@ -0,0 +1,23 @@
+package com.flexrate.flexrate_back.common.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
+@Converter(autoApply = false)
+public class YearMonthAttributeConverter implements AttributeConverter<YearMonth, String> {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
+
+    @Override
+    public String convertToDatabaseColumn(YearMonth attribute) {
+        return attribute != null ? attribute.format(FORMATTER) : null;
+    }
+
+    @Override
+    public YearMonth convertToEntityAttribute(String dbData) {
+        return dbData != null ? YearMonth.parse(dbData, FORMATTER) : null;
+    }
+}
