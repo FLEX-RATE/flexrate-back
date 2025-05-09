87d8500bd66541c7b2c6d43f43f180102c75beeb
yyc0026 <youngchan206@gmail.com>
2025-05-09 17:44:28 +0900

    MemberService commit

--- src/main/java/com/flexrate/flexrate_back/member/application/MemberService.java
+++ src/main/java/com/flexrate/flexrate_back/member/application/MemberService.java
@@ -1,9 +1,15 @@
 package com.flexrate.flexrate_back.member.application;
 
+import com.flexrate.flexrate_back.auth.domain.FidoCredential;
 import com.flexrate.flexrate_back.common.exception.ErrorCode;
 import com.flexrate.flexrate_back.common.exception.FlexrateException;
+import com.flexrate.flexrate_back.financialdata.domain.UserFinancialData;
+import com.flexrate.flexrate_back.financialdata.enums.UserFinancialCategory;
+import com.flexrate.flexrate_back.financialdata.enums.UserFinancialDataType;
 import com.flexrate.flexrate_back.member.domain.Member;
+import com.flexrate.flexrate_back.member.domain.repository.FidoCredentialRepository;
 import com.flexrate.flexrate_back.member.domain.repository.MemberRepository;
+import com.flexrate.flexrate_back.member.domain.repository.UserFinancialDataRepository;
 import com.flexrate.flexrate_back.member.dto.*;
 import com.flexrate.flexrate_back.member.enums.*;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +17,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final FidoCredentialRepository fidoCredentialRepository;
+    private final UserFinancialDataRepository financialDataRepository;
 
 
     /*
@@ -24,7 +39,6 @@ public class MemberService {
      * @since 2025.05.03
      * @author 윤영찬
      * */
-
     public SignupResponseDTO registerMember(SignupRequestDTO signupDTO) {
         if (memberRepository.existsByEmail(signupDTO.email())) {
             throw new FlexrateException(ErrorCode.EMAIL_ALREADY_REGISTERED);
@@ -32,7 +46,6 @@ public class MemberService {
 
         String rawPwd = signupDTO.password();
         String hashedPwd = passwordEncoder.encode(rawPwd);
-
 
         ConsumptionType consumptionType;
         ConsumeGoal consumeGoal;
@@ -59,14 +72,172 @@ public class MemberService {
 
         Member saved = memberRepository.save(member);
 
+        if (signupDTO.passkeys() != null && !signupDTO.passkeys().isEmpty()) {
+            savePasskeys(saved, signupDTO.passkeys());
+        }
+
+        if (signupDTO.consents() != null && !signupDTO.consents().isEmpty()) {
+            handleConsents(signupDTO.consents());
+        }
+
+        // 더미 데이터를 생성하는 코드 호출
+        generateDummyFinancialData(saved);
+
         return SignupResponseDTO.builder()
                 .userId(saved.getMemberId())
                 .email(saved.getEmail())
                 .build();
     }
 
+    private void savePasskeys(Member member, List<PasskeyRequestDTO> passkeys) {
+        for (PasskeyRequestDTO passkey : passkeys) {
+            FidoCredential fidoCredential = FidoCredential.builder()
+                    .member(member)
+                    .publicKey(passkey.publicKey())
+                    .signCount(passkey.signCount())
+                    .deviceInfo(passkey.deviceInfo())
+                    .isActive(passkey.isActive())
+                    .lastUsedDate(LocalDateTime.now())
+                    .build();
+
+            fidoCredentialRepository.save(fidoCredential);
+        }
+    }
+
+    private void handleConsents(List<ConsentRequestDTO> consents) {
+        for (ConsentRequestDTO consent : consents) {
+            System.out.println("Consent type: " + consent.type() + ", Agreed: " + consent.agreed());
+        }
+    }
+
+    private void generateDummyFinancialData(Member member) {
+        List<UserFinancialData> dummyDataList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            // UserFinancialDataType에 따라 카테고리 설정
+            UserFinancialDataType dataType = i % 3 == 0
+                    ? UserFinancialDataType.INCOME
+                    : i % 3 == 1
+                    ? UserFinancialDataType.EXPENSE
+                    : UserFinancialDataType.LOAN_BALANCE;
+
+            // 해당 유형에 맞는 카테고리 랜덤 선택
+            UserFinancialCategory category = getRandomCategoryForType(dataType);
+
+            // 더미 데이터 생성
+            UserFinancialData data = UserFinancialData.builder()
+                    .member(member)
+                    .dataType(dataType)
+                    .category(category)  // 해당 유형에 맞는 카테고리 사용
+                    .value((int) (Math.random() * 100000) + 1000)  // 1,000에서 100,000 사이의 랜덤 값
+                    .collectedAt(LocalDateTime.now().minusDays(new Random().nextInt(365)))  // 랜덤 날짜 생성
+                    .build();
+
+            dummyDataList.add(data);
+        }
+
+        // 더미 데이터 저장
+        financialDataRepository.saveAll(dummyDataList);
+    }
+
+    private UserFinancialCategory getRandomCategoryForType(UserFinancialDataType type) {
+        switch (type) {
+            case INCOME:
+                return getRandomCategoryForIncome();
+            case EXPENSE:
+                return getRandomCategoryForExpense();
+            case LOAN_BALANCE:
+                return getRandomCategoryForLoanBalance();
+            default:
+                throw new FlexrateException(ErrorCode.VALIDATION_ERROR);  // 잘못된 타입에 대해 예외 처리
+        }
+    }
+
+    private UserFinancialCategory getRandomCategoryForIncome() {
+        return getRandomCategoryFromList(UserFinancialCategory.FOOD, UserFinancialCategory.HEALTH, UserFinancialCategory.EDUCATION);
+    }
+
+    private UserFinancialCategory getRandomCategoryForExpense() {
+        return getRandomCategoryFromList(UserFinancialCategory.LIVING, UserFinancialCategory.TRANSPORT, UserFinancialCategory.LEISURE);
+    }
+
+    private UserFinancialCategory getRandomCategoryForLoanBalance() {
+        return getRandomCategoryFromList(UserFinancialCategory.COMMUNICATION, UserFinancialCategory.ETC);
+    }
+
+    private UserFinancialCategory getRandomCategoryFromList(UserFinancialCategory... categories) {
+        List<UserFinancialCategory> categoryList = Arrays.asList(categories);
+        return categoryList.get(new Random().nextInt(categoryList.size()));
+    }
+
+
     public Member findById(Long memberId) {
-        return memberRepository.findById(memberId)
+    return memberRepository.findById(memberId)
                 .orElseThrow(() -> new FlexrateException(ErrorCode.USER_NOT_FOUND));
     }
+
+    /**
+     * 마이페이지 조회
+     * @param memberId 회원 ID
+     * @return 회원 정보(MypageResponse) - 이름, 이메일, 소비 목표, 소비 유형
+     * @since 2025.05.07
+     * @author 권민지
+     */
+    public MypageResponse getMyPage(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new FlexrateException(ErrorCode.USER_NOT_FOUND));
+
+        return MypageResponse.builder()
+                .name(member.getName())
+                .email(member.getEmail())
+                .consumeGoal(member.getConsumeGoal())
+                .consumptionType(member.getConsumptionType())
+                .build();
+    }
+
+    /**
+     * 마이페이지 정보 수정
+     * @param memberId 회원 ID
+     * @param request MypageUpdateRequest 요청 DTO (이메일, 소비 목표)
+     * @return 수정된 회원 정보(MypageResponse) - 이름, 이메일, 소비 목표, 소비 유형
+     * @since 2025.05.07
+     * @author 권민지
+     */
+    @Transactional
+    public MypageResponse updateMyPage(Long memberId, MypageUpdateRequest request) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new FlexrateException(ErrorCode.USER_NOT_FOUND));
+
+        if (request.email() != null) member.updateEmail(request.email());
+        if (request.consumeGoal() != null) {
+            // L011 - 소비 목표가 소비 타입과 다르다면, 소비 목표를 변경할 수 없음
+            if (request.consumeGoal().getType() != member.getConsumptionType()) {
+                throw new FlexrateException(ErrorCode.LOAN_CONSUMPTION_TYPE_MISMATCH);
+            }
+
+            member.updateConsumeGoal(request.consumeGoal());
+        }
+
+        return MypageResponse.builder()
+                .name(member.getName())
+                .email(member.getEmail())
+                .consumeGoal(member.getConsumeGoal())
+                .consumptionType(member.getConsumptionType())
+                .build();
+    }
+
+    /**
+     * 소비 유형별 소비 목표 반환
+     * @param consumptionType 소비 유형
+     * @return 소비 목표 list
+     * @since 2025.05.07
+     * @author 권민지
+     */
+    public ConsumeGoalResponse getConsumeGoal(ConsumptionType consumptionType) {
+        return ConsumeGoalResponse.builder()
+                .consumeGoals(ConsumeGoal.getConsumeGoalsByType(consumptionType))
+                .build();
+
+    }
 }
+
