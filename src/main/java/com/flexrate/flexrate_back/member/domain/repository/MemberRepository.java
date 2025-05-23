3fb9ddbb32e9017af1b6d9b2da3b9fc1e3b94475
ryuseunghan <jorzor99@gmail.com>
2025-05-23 16:22:26 +0900

    feat : findAllWithLoanApplication 쿼리 구현

--- src/main/java/com/flexrate/flexrate_back/member/domain/repository/MemberRepository.java
+++ src/main/java/com/flexrate/flexrate_back/member/domain/repository/MemberRepository.java
@@ -2,12 +2,16 @@ package com.flexrate.flexrate_back.member.domain.repository;
 
 import com.flexrate.flexrate_back.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
-    Optional<Member> findByName(String name);
-
     Optional<Member> findByEmail(String email);
+
+    @Query("SELECT DISTINCT m FROM Member m JOIN FETCH m.loanApplication a JOIN FETCH a.product")
+    List<Member> findAllWithLoanApplication();
+
 }
