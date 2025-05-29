9a686a9a310fcdf1ea77a879985ac9bf3cdcc7b1
mjgwon24 <alswlchlrh8@naver.com>
2025-05-29 19:36:46 +0900

    feat: 로깅 식별을 위한 TraceIdInterceptor, WebConfig 설정 추가

---
+++ src/main/java/com/flexrate/flexrate_back/common/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.flexrate.flexrate_back.common.config;
+
+import com.flexrate.flexrate_back.common.interceptor.TraceIdInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final TraceIdInterceptor traceIdInterceptor;
+
+    public WebConfig(TraceIdInterceptor traceIdInterceptor) {
+        this.traceIdInterceptor = traceIdInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(traceIdInterceptor)
+                .addPathPatterns("/**");
+    }
+}
